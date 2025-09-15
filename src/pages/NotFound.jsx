4d7024bc989af1143f52c6218e19ff2fd058c53c
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 21:02:10 +0530

    Create NotFound.jsx

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+export default function NotFound() {
+  return (
+    <div>
+      <h2>404 - Page Not Found</h2>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
+}
