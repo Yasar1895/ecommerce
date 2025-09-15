2778dabd72796a0c0db4bdd43af824aabcfe0ea2
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 21:35:12 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Header() {
   const { cart } = useContext(CartContext);
@@ -8,7 +9,10 @@ export default function Header() {
     <header className="header">
       <Link to="/" className="logo">E-Shop</Link>
       <nav>
-        <Link to="/cart">Cart ({cart.length})</Link>
+        <Link to="/cart">
+          <FaShoppingCart style={{ marginRight: "5px" }} />
+          Cart ({cart.length})
+        </Link>
       </nav>
     </header>
   );
