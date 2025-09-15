bba9d00523dc1a107b47c1a7fb33e6b6bbdff0f4
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-15 21:36:07 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,14 @@ import ProductCard from "../components/ProductCard";
 
 export default function Home() {
   return (
-    <div className="home">
-      <h1>Products</h1>
-      <div className="product-grid">
-        {products.map(p => <ProductCard key={p.id} product={p} />)}
+    <div className="container">
+      <h1 className="text-center my-4">Latest Products</h1>
+      <div className="row">
+        {products.map(p => (
+          <div key={p.id} className="col-md-4 col-lg-3 mb-4">
+            <ProductCard product={p} />
+          </div>
+        ))}
       </div>
     </div>
   );
