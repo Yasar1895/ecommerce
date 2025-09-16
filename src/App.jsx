8c1cc881f057e2f3d4698f9829ce0a6ff264d045
Yasar Arafath <55400911+Yasar1895@users.noreply.github.com>
2025-09-16 11:28:08 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import ProductDetails from "./pages/ProductDetails";
 import NotFound from "./pages/NotFound";
+import { CartProvider } from "./context/CartContext";
+import "./styles.css";
 
-export default function App() {
+function App() {
   return (
     <CartProvider>
       <Router>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/product/:id" element={<ProductDetails />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
       </Router>
     </CartProvider>
   );
 }
+
+export default App;
